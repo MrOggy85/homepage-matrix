8fc359b0bdeadc33ba5d78c2a884300a549e4817
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-04-22 23:00:24 +0900

    chore: update build.sh install deno

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+echo "-----------------------"
+echo "---- Build start ------"
+echo "-----------------------"
+
+uname -a
+pwd
+
+echo $HOME
+
+# Install Deno
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+export DENO_INSTALL="/$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
 echo "Node version:"
 node -v
 echo "Deno version:"

57c671f0fb240308c82e2bce4f075f4ad5b00f74
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-04-23 00:05:06 +0900

    chore: update fathom script

--- src/fathom.js
+++ src/fathom.js
@@ -1,35 +1,35 @@
 // deno-lint-ignore-file
 
-const host = document.location.host;
-if (host !== 'www.oskarlindgren.se') {
-  return;
+function checkIfHostIsCorrect() {
+  const host = document.location.host;
+  return host === 'www.oskarlindgren.se'
 }
 
-const userAgent = navigator.userAgent.toLowerCase()
+function checkIsBot() {
+  const userAgent = navigator.userAgent.toLowerCase()
 
-const isGoogleBot = userAgent.includes('googlebot')
-const isBingBot = userAgent.includes('bingbot')
-const isYahooBot = userAgent.includes('slurp')
-const isDuckDuckBot = userAgent.includes('duckduckbot')
-const isChineseBot = userAgent.includes('spider')
-const isYandexBot = userAgent.includes('yandexbot')
-const isAlexaBot = userAgent.includes('ia_archiver')
-const isFacebookBot = userAgent.includes('facebot') || userAgent.includes('facebookexternalhit')
+  const isGoogleBot = userAgent.includes('googlebot')
+  const isBingBot = userAgent.includes('bingbot')
+  const isYahooBot = userAgent.includes('slurp')
+  const isDuckDuckBot = userAgent.includes('duckduckbot')
+  const isChineseBot = userAgent.includes('spider')
+  const isYandexBot = userAgent.includes('yandexbot')
+  const isAlexaBot = userAgent.includes('ia_archiver')
+  const isFacebookBot = userAgent.includes('facebot') || userAgent.includes('facebookexternalhit')
 
-const isBot = isGoogleBot
-  || isBingBot
-  || isYahooBot
-  || isDuckDuckBot
-  || isChineseBot
-  || isYandexBot
-  || isAlexaBot
-  || isFacebookBot
+  const isBot = isGoogleBot
+    || isBingBot
+    || isYahooBot
+    || isDuckDuckBot
+    || isChineseBot
+    || isYandexBot
+    || isAlexaBot
+    || isFacebookBot;
 
-if (isBot) {
-  return;
+  return isBot;
 }
 
-(function(f, a, t, h, o, m){
+function initFathom(f, a, t, h, o, m) {
   a[h]=a[h]||function(){
     (a[h].q=a[h].q||[]).push(arguments)
   };
@@ -37,6 +37,25 @@ if (isBot) {
   m=f.getElementsByTagName('script')[0];
   o.async=1; o.src=t; o.id='fathom-script';
   m.parentNode.insertBefore(o,m)
-})(document, window, '//monitor.oskarlindgren.se/tracker.js', 'fathom');
-fathom('set', 'siteId', 'NYKME');
-fathom('trackPageview');
+}
+
+function trackPageView() {
+  initFathom(document, window, '//monitor.oskarlindgren.se/tracker.js', 'fathom');
+  fathom('set', 'siteId', 'NYKME');
+  fathom('trackPageview');
+}
+
+function init() {
+  const hostOk = checkIfHostIsCorrect()
+  if (!hostOk) {
+    return;
+  }
+
+  const isBot = checkIsBot();
+  if (isBot) {
+    return;
+  }
+
+  trackPageView();
+}
+init();
